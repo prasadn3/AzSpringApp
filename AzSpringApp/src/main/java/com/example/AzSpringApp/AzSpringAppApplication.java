abe73590a007034d3bfbfcf4a23e5a63217bb4f2
Prasad <prasadnandyala185@gmail.com>
2025-06-18 14:53:32 +0530

    Initial commit - Spring Boot Hello World app

---
+++ AzSpringApp/src/main/java/com/example/AzSpringApp/AzSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.AzSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzSpringAppApplication.class, args);
+		System.out.println("AzSpringApp is running!");
+	}
+
+}
---
+++ AzSpringApp/src/test/java/com/example/AzSpringApp/AzSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
